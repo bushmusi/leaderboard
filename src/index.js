08bc7076789bae89eecc840c7eb55dbd7a7b7ea8
Bushra Mustofa <bushera.mestofa@gmail.com>
2022-03-12 14:12:32 +0300

    Add refresh and submit feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import './style.css';
 import './style.scss';
+import Api from './modules/api-controller.js';
+import refresh from './modules/refresh-view.js';
 
-console.log('hello webpack');
+const request = new Api();
 
-const fancyFunc = () => [1, 2];
+const displayData =async () => {
+    const data =await request.getScores();
+    refresh(data);
+}
 
-const [a, b] = fancyFunc();
+displayData()
+
+document.getElementById('refresh').addEventListener('click', async () => {
+    displayData()
+});
+
+document.getElementById('score-form').addEventListener('submit',async (e) => {
+    e.preventDefault();
+    const form = document.getElementById('score-form');
+    const { elements } = form;
+    const data = {
+        user: elements.user.value,
+        score: elements.score.value
+    }
+    await request.addScore(data);
+    form.reset();
+    displayData();
+})
